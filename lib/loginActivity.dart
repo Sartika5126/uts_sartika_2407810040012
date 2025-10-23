25d4206614fb1add933143d12fc1233d1a0b2766
unknown <sartika.lp3i.krw24@gmail.com>
2025-10-23 20:24:56 +0700

    kelima

--- lib/loginActivity.dart
+++ lib/loginActivity.dart
@@ -11,11 +11,11 @@ class Login extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(height: 60),
+            SizedBox(height: 5),
             Center(
               child:Column(
                 children: [
-                  Image.asset("gambar/Logo.png", width: 100, height: 100),
+                  Image.asset("gambar/Logo.png", width: 200, height: 200),
                   Text(
                     "LKS MART",
                     style: TextStyle(
@@ -50,12 +50,25 @@ class Login extends StatelessWidget {
                 Text(
                   "Enter your ID and password to sign in!",
                   style: TextStyle(
-                    fontSize: 15,
+                    fontSize: 12,
                     color: Colors.grey,
                   ),
                 ),
               ],
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Text(
+                    "Email"
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
