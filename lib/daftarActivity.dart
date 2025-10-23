d536814cfcb37310024780774d190b536ca9b25f
unknown <sartika.lp3i.krw24@gmail.com>
2025-10-23 22:03:50 +0700

    terakhir

--- lib/daftarActivity.dart
+++ lib/daftarActivity.dart
@@ -8,10 +8,15 @@ class Daftar extends StatefulWidget {
 }
 
 class _DaftarState extends State<Daftar> {
+  bool _showpassword = true;
+  final _namacontroller = TextEditingController();
+  final _alamatcontroller = TextEditingController();
+  final _usernamecontroller = TextEditingController();
+  final _passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(30),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -49,6 +54,161 @@ class _DaftarState extends State<Daftar> {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Silahkan isi Data Pribadi Anda",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                spacing: 8,
+                children: [
+                  Text("Nama Lengkap",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  TextField(
+                    controller: _namacontroller,
+                    decoration: InputDecoration(
+                      hintText: "Nama Lengkap",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )
+                    ),
+                  ),
+                  Text("Alamat",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  TextField(
+                    controller: _alamatcontroller,
+                    decoration: InputDecoration(
+                      hintText: "Alamat",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )
+                    ),
+                  ),
+                  Text("Username",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  TextField(
+                    controller: _usernamecontroller,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      )
+                    ),
+                  ),
+                  Text("Password*",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  TextField(
+                    controller: _passwordcontroller,
+                    obscureText: _showpassword,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showpassword = !_showpassword;
+                          });
+                        },
+                        icon: Icon(
+                          _showpassword ?Icons.remove_red_eye : Icons.visibility_off,
+                        ),
+                      ),
+                      hintText: "Min 8 characters",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  Text("Konfirmasi Pasword*",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  TextField(
+                    controller: _passwordcontroller,
+                    obscureText: _showpassword,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showpassword = !_showpassword;
+                          });
+                        },
+                        icon: Icon(
+                          _showpassword ?Icons.remove_red_eye : Icons.visibility_off,
+                        ),
+                      ),
+                      hintText: "Min 8 characters",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(double.infinity, 50),
+                        backgroundColor: Colors.blue,
+                      ), 
+                      child: Text(
+                        "Daftar",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Sudah Punya Akun? "),
+                  Text("Login di sini",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),)
+                ],
+              ),
+            )
           ],
         ),
       ),
