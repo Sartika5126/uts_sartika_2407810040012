50fa9ba7028b659d6718883597c1ea9169dcf75f
unknown <sartika.lp3i.krw24@gmail.com>
2025-10-23 19:29:48 +0700

    kedua

--- lib/loginActivity.dart
+++ lib/loginActivity.dart
@@ -6,7 +6,29 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+         padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 60),
+            Column(
+              children: [
+                Image.asset("gambar/Logo.png", width: 100, height: 100),
+                Text(
+                  "LKS MART",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+
+            )
+          ],
+        ),
+      ),
     );
   }
 }
